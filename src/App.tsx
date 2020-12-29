ab7f06399c9daa709f00e982bb44978dd3493ea2
Bartosz Nowicki <b_nowicki@op.pl>
2020-12-29 21:59:04 +0100

    sheriff sliding left and right

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import './App.scss';
+import { Sheriff } from './components/Sheriff';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainWrap">    
+      <div className="board">
+        <Sheriff status = {0}/>    
+      </div>    
     </div>
   );
 }
 
 export default App;
+
+
+// ORIG
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Sheriff.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import './sheriff.scss';
+
+export const Sheriff: React.FC<{status: number}> = ({status})  => {
+    const [count, setcount] = useState<number>(status);
+    const [posX, setposX] = useState<number>(status);
+    // console.log('SHERIFF RENDER NR: ', count);
+    // console.log('CURRENT POSITION X: ', posX);  
+    
+    const moveLeft = (x: number) =>  setposX(posX => posX - x);
+    const moveRight = (x: number) => setposX(posX => posX + x);
+    
+    function handleKeyPress(e: any) {
+        const { key } = e;  //instead of e.key
+        const increment = 50;
+        console.log('key: ', key);        
+            switch (key) {
+                case 'ArrowLeft':
+                    moveLeft(increment); 
+                break;
+                case 'ArrowRight':
+                    moveRight(increment); 
+                break;
+                      
+                default:
+                  console.log(`PRESS A FUNCTION KEY INSTEAD OF: ${key}...`);
+              }      
+    };
+
+    useEffect(() => {        
+        window.addEventListener('keyup', handleKeyPress);  
+        console.log('CONTROLSLISTENER ASSIGNED');
+        //cleanup to remove listener before re-rendering not necessary if empty dependency []
+        //cleanup would look like so:
+        //return window.removeEventListener('keyup', handleKeyPress);    
+    }, []);    
+
+    // useEffect(() => {
+    //     console.log('CURRENT POS X: ', posX);        
+    // }, []);   
+    
+
+    const handleClick = () => {
+        setcount(count => count + 1);
+        console.log('SHERIFF CLICKED, RE-RENDER REQUESTED ');
+    }   
+
+    const sheriffStyle = {
+        transform: `translateX(${posX}px)`
+    };
+
+
+
+    return (
+        <div className="sheriffDiv" style = {sheriffStyle} onClick = {() => handleClick()}>
+          <p>{count} : {posX}</p>
+        </div>
+    )
+}
+
+
